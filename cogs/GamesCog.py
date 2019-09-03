d7d386afb0a7aa166a7346c9e60c506a8622362e
Ryan Phillips <rcphillips15@gmail.com>
2019-09-03 18:57:52 -0500

    add embed to when command

--- cogs/GamesCog.py
+++ cogs/GamesCog.py
@@ -5,6 +5,43 @@ from discord.ext import commands
 import discord
 from .utils import time_delta_str_tuple
 
+async def async_query_igdb(e_point: str, target: str, filters: str) -> tuple:
+  '''
+  Makes an ansynchronous network request to an igdb endpoint
+
+  Returns a tuple containg two items (RequestResponse, Error)
+
+  @params:
+    e_point - the specific endpoint for igdb request
+    target - the item you are searching for in the end point
+    filters - comma seperated values used to filter the result
+
+  @igdb docs:
+    https://api-docs.igdb.com
+
+  '''
+  if not (isinstance(target, str) and
+          isinstance(e_point, str) and
+          isinstance(filter, str)):
+    return {}, TypeError('All arguments must be strings')
+
+  if not target or not e_point:
+    # no game was specified
+    return {}, ValueError('Target or endpoint is empty')
+
+  url = f'https://api-v3.igdb.com/{e_point}?search={target}&fields={filters}'
+  headers = {'user-key': environ.get('IGDB_KEY')}
+  async with ClientSession(headers=headers, raise_for_status=False) as sess:
+    try:
+      async with sess.get(url, raise_for_status=True) as res:
+        data = loads(await res.read())[0]
+    except (ClientError, IndexError) as ex:
+      if not sess.closed:
+        await sess.close()
+      return {}, ex
+    else:
+      return data, None
+
 class GamesCog(commands.Cog, name='Game'):
   '''Game Cog - Commands for game related information'''
   def __init__(self, bot, **options):
@@ -62,56 +99,35 @@ class GamesCog(commands.Cog, name='Game'):
       ctx.send('when command example -> !when borderlands 3')
       return
 
-    member = member if member is not None else ctx.author
     game_name = str(' ').join(args)
-    game, err = await async_query_igdb('games', game_name, 'name,first_release_date')
-
+    member = member if member is not None else ctx.author
+    filters = 'name,first_release_date,cover.url'
+    
+    game, err = await async_query_igdb('games', game_name, filters)
     self._update_state('date', game_name, game, member, bool(err))
-
     timestamp = game.get('first_release_date', False)
+
     if err is not None or not timestamp:
       await ctx.send(f'Sorry, I was unable to find the release date for {game_name}')
       return
 
     days, hours, mins, secs = time_delta_str_tuple(timestamp)
-    msg = f'{game_name} releases in {days}, {hours} hours, {mins} mins, and {secs} seconds!'
-    await ctx.send(msg)
 
+    if int(days.split(' ')[0]) < 0:
+      ctx.send(f'{game_name} has already been released!')
+      return
 
-async def async_query_igdb(e_point: str, target: str, filters: str) -> tuple:
-  '''
-  Makes an ansynchronous network request to an igdb endpoint
+    countdown = f'{days}, {hours} hours, {mins} mins, and {secs} seconds!'
+    game_pic = game.get('cover', False)
 
-  Returns a tuple containg two items (RequestResponse, Error)
-
-  @params:
-    e_point - the specific endpoint for igdb request
-    target - the item you are searching for in the end point
-    filters - comma seperated values used to filter the result
-
-  @igdb docs:
-    https://api-docs.igdb.com
-
-  '''
-  if not (isinstance(target, str) and
-          isinstance(e_point, str) and
-          isinstance(filter, str)):
-    return {}, TypeError('All arguments must be ')
-
-  if not target or not e_point:
-    # no game was specified
-    return {}, ValueError('Target or endpoint was empty')
-
-  url = f'https://api-v3.igdb.com/{e_point}?search={target}&fields={filters}'
-  headers = {'user-key': environ.get('IGDB_KEY')}
-
-  async with ClientSession(headers=headers, raise_for_status=False) as sess:
-    try:
-      async with sess.get(url, raise_for_status=True) as res:
-        data = loads(await res.read())[0]
-    except (ClientError, IndexError) as ex:
-      if not sess.closed:
-        await sess.close()
-      return {}, ex
+    if not game_pic:
+      await ctx.send(f'{game_name} releases in {countdown}')
     else:
-      return data, None
+      game_pic_url = game_pic['url']
+      embed = discord.Embed(title=game_name + ' countdown:',
+                            description=countdown,
+                            colour=1024228,
+                            type='rich')
+
+      embed.set_footer(text=game_name, icon_url=game_pic_url)
+      await ctx.send(embed)
